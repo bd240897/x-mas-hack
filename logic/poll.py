09c5ffe69b23e687e09645c141a58203c6571cdc
bd240897 <bd240897@yandex.ru>
2022-12-18 15:38:19 +0300

    refactoring

--- logic/poll.py
+++ logic/poll.py
@@ -1,92 +1,94 @@
-import streamlit as st
-
-
-def calculate_user_status(answers: list):
-    """Получает инвест профиль пользователя"""
-    """Заглушка"""
-
-    """
-    max - 16
-    min 4
-    Пороги
-    Консервативный  <8
-    Умеренный <12
-    Рискованный >=13
-    """
-
-    answers_sum = sum(answers)
-
-    if answers_sum < 8:
-        user_status = "Консервативный"
-    elif answers_sum < 12:
-        user_status = "Умеренный"
-    else:
-        user_status = "Рискованный"
-    return user_status
-
-def send_poll():
-    """Отправить опрос"""
-    st.session_state['IS_POLL_SHOW'] = False
-
-def poll_questions():
-    """Генерация опроса"""
-
-    answers_list = []
-    answers_weight_list = []
-
-    # ОПРОС
-    answers_task_1 = ['до 1 года', 'до 3-х лет', 'до 5-ти лет', 'до 10-ти лет']
-    weight_task_1 = [1, 2, 3, 4]
-    labels_task_1 = range(4)
-    task_1 = st.radio(
-        label=f"На какой срок вы планируете инвестировать?",
-        options=labels_task_1,
-        format_func=lambda i: answers_task_1[i]
-    )
-    answers_list.append(task_1)
-    answers_weight_list.append(weight_task_1[task_1])
-
-    answers_task_2 = ('переживаю', 'нейтральной', 'не переживаю')
-    weight_task_2 = [1, 2, 3]
-    labels_task_2 = range(3)
-    task_2 = st.radio(
-        label="Как вы относитесь к кратковременным просадкам в вашем портфеле?",
-        options=labels_task_2,
-        format_func=lambda i: answers_task_2[i]
-    )
-    answers_list.append(task_2)
-    answers_weight_list.append(weight_task_2[task_2])
-
-    answers_task_3 = ('18-27 лет', '27-45 лет', '45-65 лет', '65 лет')
-    weight_task_3 = [4, 3, 2, 1]
-    labels_task_3 = range(4)
-    task_3 = st.radio(
-        label="К какой возрастной группу вы относитесь?",
-        options=labels_task_3,
-        format_func=lambda i: answers_task_3[i]
-    )
-    answers_list.append(task_3)
-    answers_weight_list.append(weight_task_3[task_3])
-
-    answers_task_4 = ['не имею опыта', 'меньше года', 'больше года']
-    weight_task_4 = [1, 2, 3]
-    labels_task_4 = range(3)
-    task_4 = st.radio(
-        label="Какой опыт вы имеете в инвестировании?",
-        options=labels_task_4,
-        format_func=lambda i: answers_task_4[i]
-    )
-    answers_list.append(task_4)
-    answers_weight_list.append(weight_task_4[task_4])
-
-    # increment = st.button('Отправить')
-    #
-    # if increment:
-    #     st.write('Итоги опроса:')
-    #     st.text(f"1 -{task_1}, \n"
-    #             f"2 -{task_2}, \n"
-    #             f"3 -{task_3}. \n"
-    #             f"4 -{task_4}. \n")
-
-    st.session_state['answers_list'] = answers_list
-    st.session_state['answers_weight_list'] = answers_weight_list
+# import streamlit as st
+#
+#
+# def calculate_user_status(answers: list):
+#     """Получает инвест профиль пользователя"""
+#     """Заглушка"""
+#
+#     """
+#     max - 16
+#     min 4
+#     Пороги
+#     Консервативный  <8
+#     Умеренный <12
+#     Рискованный >=13
+#     """
+#
+#     answers_sum = sum(answers)
+#
+#     if answers_sum < 8:
+#         user_status = "Консервативный"
+#     elif answers_sum < 12:
+#         user_status = "Умеренный"
+#     else:
+#         user_status = "Рискованный"
+#     return user_status
+#
+#
+# def send_poll():
+#     """Отправить опрос"""
+#     st.session_state['IS_POLL_SHOW'] = False
+#
+#
+# def poll_questions():
+#     """Генерация опроса"""
+#
+#     answers_list = []
+#     answers_weight_list = []
+#
+#     # ОПРОС
+#     answers_task_1 = ['до 1 года', 'до 3-х лет', 'до 5-ти лет', 'до 10-ти лет']
+#     weight_task_1 = [1, 2, 3, 4]
+#     labels_task_1 = range(4)
+#     task_1 = st.radio(
+#         label=f"На какой срок вы планируете инвестировать?",
+#         options=labels_task_1,
+#         format_func=lambda i: answers_task_1[i]
+#     )
+#     answers_list.append(task_1)
+#     answers_weight_list.append(weight_task_1[task_1])
+#
+#     answers_task_2 = ('переживаю', 'нейтральной', 'не переживаю')
+#     weight_task_2 = [1, 2, 3]
+#     labels_task_2 = range(3)
+#     task_2 = st.radio(
+#         label="Как вы относитесь к кратковременным просадкам в вашем портфеле?",
+#         options=labels_task_2,
+#         format_func=lambda i: answers_task_2[i]
+#     )
+#     answers_list.append(task_2)
+#     answers_weight_list.append(weight_task_2[task_2])
+#
+#     answers_task_3 = ('18-27 лет', '27-45 лет', '45-65 лет', '65 лет')
+#     weight_task_3 = [4, 3, 2, 1]
+#     labels_task_3 = range(4)
+#     task_3 = st.radio(
+#         label="К какой возрастной группу вы относитесь?",
+#         options=labels_task_3,
+#         format_func=lambda i: answers_task_3[i]
+#     )
+#     answers_list.append(task_3)
+#     answers_weight_list.append(weight_task_3[task_3])
+#
+#     answers_task_4 = ['не имею опыта', 'меньше года', 'больше года']
+#     weight_task_4 = [1, 2, 3]
+#     labels_task_4 = range(3)
+#     task_4 = st.radio(
+#         label="Какой опыт вы имеете в инвестировании?",
+#         options=labels_task_4,
+#         format_func=lambda i: answers_task_4[i]
+#     )
+#     answers_list.append(task_4)
+#     answers_weight_list.append(weight_task_4[task_4])
+#
+#     # increment = st.button('Отправить')
+#     #
+#     # if increment:
+#     #     st.write('Итоги опроса:')
+#     #     st.text(f"1 -{task_1}, \n"
+#     #             f"2 -{task_2}, \n"
+#     #             f"3 -{task_3}. \n"
+#     #             f"4 -{task_4}. \n")
+#
+#     st.session_state['answers_list'] = answers_list
+#     st.session_state['answers_weight_list'] = answers_weight_list
--- pages/1-poll.py
+++ pages/1-poll.py
@@ -1,37 +1,102 @@
 import streamlit as st
-from logic.poll import calculate_user_status, poll_questions
+# from logic.poll import calculate_user_status
 import os
+import json
 
-st.title('Это страница опроса :sunglasses:')
+# НАЧАЛЬНЫЕ УСЛОВИЯ
 
-try:
-    a = st.session_state['IS_POLL_SHOW']
-except:
-    st.session_state['IS_POLL_SHOW'] = 'True'
+# первоначальная инициализация
+if 'is_poll_show' not in st.session_state:
+    st.session_state['is_poll_show'] = True
 
+# ИСПОЛЬЩУЕМЫЕ ФУНКЦИИ
 
-# on click - callback on_change
 def send_poll():
-    print(st.session_state.answers_weight_list)
-    st.session_state['IS_POLL_SHOW'] = False
+    """Обратный вызов для кнопки отравить опрос"""
+
+    st.session_state['is_poll_show'] = False
     user_status = calculate_user_status(st.session_state.answers_weight_list)
     st.session_state['user_status'] = user_status
 
 
 def show_poll():
-    st.session_state['IS_POLL_SHOW'] = True
+    """Обратный вызова для кнопки показать опрос"""
+
+    st.session_state['is_poll_show'] = True
 
 
 def go_to_portfolio():
+    """Обратный вызова для кнопки перейти к портфолио"""
+
     pass
 
-if st.session_state['IS_POLL_SHOW']:
+
+def poll_questions():
+    """Генерация опроса"""
+
+    answers_list = []
+    answers_weight_list = []
+
+    with open('questions.json', encoding='utf-8') as f_in:
+        questions = json.load(f_in)
+
+    def poll_item(question: str, answers: list, weight: list):
+        """Один элемент опроса"""
+
+        labels_poll = range(len(answers))  # range(4)
+
+        user_answer = st.radio(
+            label=question,
+            options=labels_poll,
+            format_func=lambda i: answers[i]
+        )
+        answers_list.append(user_answer)
+        answers_weight_list.append(weight[user_answer])
+
+    for item in questions:
+        poll_item(item["question"], item["answers"], item["weight_poll"])
+
+    st.session_state['answers_list'] = answers_list
+    st.session_state['answers_weight_list'] = answers_weight_list
+
+
+def calculate_user_status(answers: list):
+    """Получает инвест профиль пользователя"""
+    """
+    max - 16
+    min 4
+    Пороги
+    Консервативный  <8
+    Умеренный <12
+    Рискованный >=13
+    """
+
+    answers_sum = sum(answers)
+
+    if answers_sum < 8:
+        user_status = "Консервативный"
+    elif answers_sum < 12:
+        user_status = "Умеренный"
+    else:
+        user_status = "Рискованный"
+    return user_status
+
+# КОД СТРАНИЦЫ
+
+# заголовок
+st.title('Это страница опроса :sunglasses:')
+
+if st.session_state.is_poll_show:
     st.write('Пожалуйста пройдите опрос')
     poll_questions()
-    button_1 = st.button('Отравить', on_click=send_poll)
+    st.button('Отравить', on_click=send_poll)
 else:
     st.header(f"Ваш статус:")
     st.header(f":blue[{st.session_state.user_status}]")
-    # button_1 = st.button('Перейти к портфелю', on_click=go_to_portfolio)
-    button_1 = st.button('Пройти опрос еще раз', on_click=show_poll)
+    st.button('Перейти к портфелю', on_click=go_to_portfolio)
+    st.button('Пройти опрос еще раз', on_click=show_poll)
 
+# st.button('session_state', on_click=send_poll, key='my_button')
+# st.markdown(f"""
+# #### {st.session_state}
+# """)
